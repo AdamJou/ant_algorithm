e0f136098c3887f8780745a9af250a4f4e834496
Adam Michoń <adammplan@gmail.com>
2024-06-08 18:00:58 +0200

    Next city index fix

--- src/components/AntAlgorithm.js
+++ src/components/AntAlgorithm.js
@@ -1,13 +1,9 @@
-// Plik: AntColonyOptimization.js
-
-// Funkcja obliczająca odległość między dwoma miastami na podstawie współrzędnych
 export function distance(city1, city2) {
   const dx = city1.x - city2.x;
   const dy = city1.y - city2.y;
   return Math.sqrt(dx * dx + dy * dy);
 }
 
-// Implementacja algorytmu mrówkowego
 export function antColonyOptimization(
   cities,
   numAnts,
@@ -17,75 +13,74 @@ export function antColonyOptimization(
   beta,
   startIndex
 ) {
-  // Inicjalizacja feromonów
   let pheromones = Array(cities.length)
     .fill()
     .map(() => Array(cities.length).fill(1));
 
-  // Inicjalizacja najlepszej trasy i jej długości
   let bestTour = [];
   let bestTourLength = Infinity;
 
-  // Główna pętla algorytmu
-  for (let i = 0; i < numIterations; i++) {
-    // Rozpoczęcie każdej mrówki z miasta startowego
+  for (let iteration = 0; iteration < numIterations; iteration++) {
     const ants = Array(numAnts)
       .fill()
       .map(() => {
         return {
-          tour: [startIndex], // Rozpoczynamy trasę od miasta startowego
-          visited: new Set([startIndex]), // Miasto startowe jest odwiedzone od początku
+          tour: [startIndex],
+          visited: new Set([startIndex]),
           tourLength: 0,
         };
       });
 
-    // Iteracja przez wszystkie miasta
     for (const ant of ants) {
       while (ant.tour.length < cities.length) {
         const currentCity = ant.tour[ant.tour.length - 1];
-        const unvisitedCities = cities.filter(
-          (city, index) => !ant.visited.has(index)
-        );
-        const probabilities = unvisitedCities.map((city, index) => {
+        const unvisitedCitiesIndices = cities
+          .map((city, index) => index)
+          .filter((index) => !ant.visited.has(index));
+        const probabilities = unvisitedCitiesIndices.map((index) => {
           const pheromone = pheromones[currentCity][index];
-          const dist = distance(cities[currentCity], city);
+          const dist = distance(cities[currentCity], cities[index]);
           return Math.pow(pheromone, alpha) * Math.pow(1 / dist, beta);
         });
         const sumProbabilities = probabilities.reduce(
           (acc, val) => acc + val,
           0
         );
-        const cumulatedProbabilities = probabilities.map(
+        const normalizedProbabilities = probabilities.map(
           (val) => val / sumProbabilities
         );
         const random = Math.random();
-        let selectedCityIndex = 0;
         let cumulativeProbability = 0;
-        for (let i = 0; i < cumulatedProbabilities.length; i++) {
-          cumulativeProbability += cumulatedProbabilities[i];
+        let selectedCityIndex = -1;
+        for (let i = 0; i < normalizedProbabilities.length; i++) {
+          cumulativeProbability += normalizedProbabilities[i];
           if (random < cumulativeProbability) {
-            selectedCityIndex = i;
+            selectedCityIndex = unvisitedCitiesIndices[i];
             break;
           }
         }
-        const selectedCity = unvisitedCities[selectedCityIndex];
-        ant.tour.push(cities.indexOf(selectedCity));
-        ant.visited.add(cities.indexOf(selectedCity));
-        ant.tourLength += distance(cities[currentCity], selectedCity);
+        if (selectedCityIndex === -1) {
+          selectedCityIndex =
+            unvisitedCitiesIndices[unvisitedCitiesIndices.length - 1];
+        }
+        ant.tour.push(selectedCityIndex);
+        ant.visited.add(selectedCityIndex);
+        ant.tourLength += distance(
+          cities[currentCity],
+          cities[selectedCityIndex]
+        );
       }
-      // Dodajemy odległość między ostatnim a pierwszym miastem (zamykamy pętlę)
+
       ant.tourLength += distance(
         cities[ant.tour[ant.tour.length - 1]],
         cities[startIndex]
       );
-      // Aktualizacja najlepszej trasy
+
       if (ant.tourLength < bestTourLength) {
-        bestTour = ant.tour;
+        bestTour = ant.tour.slice();
         bestTourLength = ant.tourLength;
       }
     }
-
-    // Aktualizacja feromonów
     for (let i = 0; i < cities.length; i++) {
       for (let j = 0; j < cities.length; j++) {
         pheromones[i][j] *= 1 - evaporationRate;
